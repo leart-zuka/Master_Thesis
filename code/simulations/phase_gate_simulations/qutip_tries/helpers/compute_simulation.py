40c57b8f0e8dd87f8828ecb41064333fe50dbaac
Leart Zuka <leart.zuka@web.de>
2025-09-17 19:37:32 +0200

    feat: include new way of simulating system with also H_0 included in hamiltonian which enables us to correctly simulate our shit

--- code/simulations/phase_gate_simulations/qutip_tries/helpers/compute_simulation.py
+++ code/simulations/phase_gate_simulations/qutip_tries/helpers/compute_simulation.py
@@ -63,6 +63,75 @@ def simulate(
     )
 
 
+def simulate_w_H0(
+    initial_atom_state: qt.Qobj,
+    projection_operators: Dict[Tuple[int, int], qt.Qobj],
+    annihilation_operators: Dict[str, qt.Qobj],
+    Photon_dimensions: List[int],
+    Photon_polarization: str,
+    G_pi_KC: float,
+    Delta_a: float,
+    Delta_c: float,
+    real_input_shape: Callable[[float, Dict[str, float]], float],
+    tlist: np.ndarray[Any, np.dtype[np.float32]],
+    c_ops: List[qt.Qobj],
+    observables: List[qt.Qobj],
+    args: Dict[str, float],
+) -> qt.Result:
+    """
+    Simulates the time evolution of a quantum system consisting of an atom interacting
+    with a single mode of the electromagnetic field, driven by a polarized light pulse.
+    As opposed to the regular simulate function, this one allows you to include the detunings
+    between the cavity mode/atomic transition and the driving field.
+
+    Parameters:
+        initial_atom_state (qt.Qobj): Initial quantum state of the atom.
+        projection_operators (Dict[Tuple[int, int], qt.Qobj]): Dictionary of atomic projection operators,
+            indexed by state transitions (e.g., (1, 4)).
+        annihilation_operators (Dict[str, qt.Qobj]): Dictionary of annihilation operators for photon modes
+            (e.g., {"a0": ..., "a1": ...}).
+        Photon_dimensions (List[int]): List of Hilbert space dimensions for each photon mode.
+        Photon_polarization (str): Key for selecting the driven polarization mode (e.g., "a0").
+        G_pi_KC (float): Jaynes–Cummings coupling strength between the atom and the cavity field.
+        Kappa_oc (float): Decay rate of the optical cavity.
+        Delta_a (float): Detuning between driving field and atomic transition
+        Delta_c (float): Detuning between driving filed and cavity mode
+        real_input_shape (Callable[[float, Dict[str, float]], float]): Time-dependent function representing
+            the envelope of the input pulse.
+        tlist (np.ndarray): Array of time points at which to solve the system.
+        c_ops (List[qt.Qobj]): List of collapse operators representing system losses.
+        observables (List[qt.Qobj]): List of observables to record during simulation.
+        args (Dict[str, float]): Dictionary of arguments passed to time-dependent functions.
+
+    Returns:
+        qt.Result: Result object containing the system evolution, including expectation values,
+        states, and other simulation data.
+    """
+    photon_states = [qt.basis(dim, 0) for dim in Photon_dimensions]
+    initial_states = [initial_atom_state] + photon_states
+    psi0 = qt.tensor(initial_states)
+    H_0 = (
+        Delta_a * projection_operators[(4, 1)] * projection_operators[(1, 4)]
+        + Delta_c * annihilation_operators["a0"].dag() * annihilation_operators["a0"]
+    )
+    H_jc = G_pi_KC * projection_operators[(1, 4)] * annihilation_operators["a0"].dag()
+    H_drive = (
+        annihilation_operators[Photon_polarization]
+        + annihilation_operators[Photon_polarization].dag()
+    )
+    H = [H_0 + H_jc + H_jc.dag(), [H_drive, real_input_shape]]
+
+    return qt.mesolve(
+        H,
+        psi0,
+        tlist,
+        c_ops,
+        observables,
+        args,
+        options=qt.Options(store_states=True, progress_bar="enhanced"),
+    )
+
+
 def simulate_v2(
     sign: int,
     initial_atom_state: qt.Qobj,
---
+++ code/simulations/phase_gate_simulations/qutip_tries/reflection_bare.py
@@ -0,0 +1,109 @@
+import numpy as np
+from helpers.generic_cavity_operators import CavityQEDSystem
+from helpers.input_shapes import real_input_shape, input_shape
+from helpers.compute_simulation import simulate, compute_output_field, simulate_w_H0
+from helpers.plotting import plot_qswitch_dynamics
+import matplotlib.pyplot as plt
+import qutip as qt
+import warnings
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+# -----------------------
+# ------ Constants ------
+# -----------------------
+Mu0 = -np.sqrt(1 / 24)  # pi (mf2 -> mf2) GG coefficient for F=2 m_f=-1 -> F'=2 m_f=-1
+Mu1 = -np.sqrt(1 / 3)  # pi (mf0 -> mf0) GG coefficient for F=2 m_f= 0 -> F'=1 m_f= 0
+
+G0_kc = 2 * np.pi * 0.032  # coupling strength (F2mf2 -> F'2mf2); measured myself :D
+G_pi_KC = G0_kc * (Mu1 / Mu0)  # coupling strength (F2mf0 -> F'1mf0)
+
+Kappa = 2 * np.pi * 0.063  # cavity dissipation rate
+Kappa_oc = 2 * np.pi * 0.054
+Gamma_5P32_5S = 2 * np.pi * 0.006065 / 2  # atom dissipation rate
+Gamma_b = 2 * np.pi * 0.001 / 2
+
+Mu_rf = 1
+Mu_fc = 0.9
+
+Delta_c = 2 * np.pi * 0.0  # [GHz]
+# Delta_c = 2 * np.pi * 0.5  # Light isn't resonant with cavity aka light is V polarized
+Delta_a = 2 * np.pi * 0.0  # [GHz]
+# Delta_a = 2 * np.pi * 6.835  # Atom isn't coupled to cavity aka atom in |0>
+
+
+Atom_dimensions = 5  # |F=1,m_f=0>,|F=2,m_f=0>,|F=2,m_f=-1>,|F=2,m_f=+1>,|F'=3,m_f=0>
+Photon_dimensions = [2]  # only π-pol. light is able to enter our cavity
+
+tlist = np.linspace(0, 1000, 10000, dtype=np.float32)
+args = {"t0": 500, "tau": 70.0, "tau_start": 91.0, "sigma": 1.0}
+
+# -----------------------
+# ---- System Params-----
+# -----------------------
+
+qced = CavityQEDSystem(
+    photon_dimensions=Photon_dimensions, atom_dimensions=Atom_dimensions
+)
+
+obs = [
+    qced.projection_operators[(0, 0)],  # |F=1,m_f=0><F=1,m_f=0|
+    qced.projection_operators[(1, 1)],  # |F=2,m_f=0><F=2,m_f=0|
+    qced.projection_operators[(4, 4)],  # |F'=3,m_f=0><F'=3,m_f=0|
+    qced.annihilation_operators["a0"].dag()
+    * qced.annihilation_operators["a0"],  # a.dag*a=n
+    qced.annihilation_operators["a0"],
+]
+
+c_obs = [
+    np.sqrt(2 * Kappa) * qced.annihilation_operators["a0"],
+    np.sqrt(2 * Gamma_b * 1 / 10)
+    * qced.projection_operators[(2, 4)],  # |F=2,m_f=-1><F'=3,m_f=0|
+]
+
+result_0 = simulate_w_H0(
+    qced.atomic_states[1],
+    qced.projection_operators,
+    qced.annihilation_operators,
+    Photon_dimensions,
+    "a0",
+    G_pi_KC,
+    # Kappa_oc,
+    Delta_a,
+    Delta_c,
+    input_shape,
+    tlist,
+    c_obs,
+    obs,
+    args,
+)
+
+a_in_t = [input_shape(t, args) for t in tlist]
+a_out_t = a_in_t + np.sqrt(Kappa_oc) * result_0.expect[-1]
+
+plt.figure()
+plt.plot(tlist, result_0.expect[-2], label="Tot")
+plt.xlabel("t")
+plt.ylabel("|⟨a⟩|")
+plt.legend()
+plt.title("Intracavity field amplitude")
+
+plt.figure()
+plt.plot(tlist, a_in_t, label="In")
+# plt.plot(tlist, a_out_t, label="Out")
+# plt.plot(tlist, a_out_t.real + a_out_t.imag, label="Out")
+plt.plot(tlist, np.imag(a_out_t), label="Im(Out)")
+plt.plot(tlist, np.real(a_out_t), label="Re(Out)")
+plt.xlabel("t")
+plt.ylabel("a")
+plt.legend()
+plt.title("Input and Output Field amplitudes")
+
+plt.figure()
+plt.plot(tlist, result_0.expect[1])
+plt.xlabel("t")
+plt.ylabel("⟨σ_z⟩")
+plt.title("Atomic inversion")
+
+
+plt.show()
