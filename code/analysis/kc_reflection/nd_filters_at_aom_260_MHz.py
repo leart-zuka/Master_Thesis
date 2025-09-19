7e2c19a592f8a783540c153bb719aa4f38ec94a4
Leart Zuka <leart.zuka@web.de>
2025-09-19 13:16:57 +0200

    feat: update pulse length calculations for aom@260MHz

--- code/analysis/kc_reflection/nd_filters_at_aom_260_MHz.py
+++ code/analysis/kc_reflection/nd_filters_at_aom_260_MHz.py
@@ -1,4 +1,8 @@
-from helpers.basic_calculations import calculate_total_reduction, calculate_power
+from helpers.basic_calculations import (
+    calculate_total_reduction,
+    calculate_power,
+    calculate_pulse_length,
+)
 from prefixed import Float
 
 offset = 11.40e-9
@@ -10,12 +14,13 @@ total_reduction, reduction_1, reduction_2 = calculate_total_reduction(
     output_no_filters, output_nd_1, output_nd_2, offset
 )
 
-frequency = 384228.115e9  # Rb87 D2 line F=2 <-> F'=3
+frequency_2_3 = 384228.115e9  # Rb87 D2 line F=2 <-> F'=3
+frequency_2_1 = 384228.686e9  # Rb87 D2 line F=2 <-> F'1
 mean_photon_number = 0.2
-pulse_length = 0.7e-6
+power_reflected_from_cavity = 60.5e-9 * total_reduction
 
-required_power = (
-    calculate_power(frequency, mean_photon_number, pulse_length) / total_reduction
+required_pulse_length = calculate_pulse_length(
+    frequency_2_1, mean_photon_number, power_reflected_from_cavity
 )
 
-print(f"Required power: {Float(required_power):.2h}")
+print(f"Required power: {Float(required_pulse_length):.2h}")
