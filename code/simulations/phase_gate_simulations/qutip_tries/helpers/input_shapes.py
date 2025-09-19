924a61528630ecc97029e8e5ed9df22a9b00b0fc
Leart Zuka <leart.zuka@web.de>
2025-09-19 19:18:30 +0200

    feat: refactor amplitude of input shape for qutip sims to be variable

--- code/simulations/phase_gate_simulations/qutip_tries/helpers/input_shapes.py
+++ code/simulations/phase_gate_simulations/qutip_tries/helpers/input_shapes.py
@@ -4,7 +4,8 @@ from typing import Dict
 
 def input_shape(t: float, args: Dict[str, float]) -> float:
     t0 = args["t0"]
-    return 0.05 * np.exp(-((t - t0 / 2) ** 2) / (t0 / 5) ** 2)
+    amp = args["amp"]
+    return amp * np.exp(-((t - t0 / 2) ** 2) / (t0 / 5) ** 2)
 
 
 def real_input_shape(t: float, args: Dict[str, float]) -> float:
