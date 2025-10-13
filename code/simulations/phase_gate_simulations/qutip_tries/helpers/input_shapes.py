a3df4a074e555347f4ef59ccf517e09e3edf8b55
Leart Zuka <leart.zuka@web.de>
2025-10-13 20:04:38 +0200

    feat: let input_shape also return full array when passed 1D time array

--- code/simulations/phase_gate_simulations/qutip_tries/helpers/input_shapes.py
+++ code/simulations/phase_gate_simulations/qutip_tries/helpers/input_shapes.py
@@ -1,8 +1,11 @@
 import numpy as np
 from typing import Dict
+from typing import Union, Callable
 
 
-def input_shape(t: float, args: Dict[str, float]) -> float:
+def input_shape(
+    t: Union[float, np.ndarray], args: Dict[str, float]
+) -> Union[float, np.ndarray]:
     t0 = args["t0"]
     amp = args["amp"]
     return amp * np.exp(-((t - t0 / 2) ** 2) / (t0 / 5) ** 2)
@@ -16,3 +19,14 @@ def real_input_shape(t: float, args: Dict[str, float]) -> float:
     pulse = np.exp(-time_shifted / tau) * (1 - np.exp(-time_shifted / tau_start)) ** 4
     pulse = pulse * (t >= t0)  # Apply Heaviside
     return pulse
+
+
+def normalized_input_shape(
+    t_list: np.ndarray,
+    shape: Callable[[np.ndarray, Dict[str, float]], float],
+    args: Dict[str, float],
+) -> np.ndarray:
+    y = shape(t_list, args)
+    area = np.trapezoid(y, t_list)
+    y_norm = y / area
+    return y_norm
