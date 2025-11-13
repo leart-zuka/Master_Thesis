1e9f0075cf6c2a786815e9de6920fc8df682b9d8
Leart Zuka <leart.zuka@web.de>
2025-11-13 19:40:41 +0100

    feat: account for complex values in matrix normalization

--- code/simulations/phase_gate_simulations/qutip_tries/helpers/generic_computations.py
+++ code/simulations/phase_gate_simulations/qutip_tries/helpers/generic_computations.py
@@ -3,7 +3,12 @@ import numpy as np
 
 def normalize_matrix(input_matrix: np.ndarray):
     normalized = np.zeros_like(input_matrix)
+    normalization_constants = normalizations(input_matrix)
+    normalized = input_matrix / normalization_constants
+    return normalized
+
+
+def normalizations(input_matrix: np.ndarray):
     col_sums = input_matrix.sum(axis=0)
     col_sums[col_sums == 0] = 1.0
-    normalized = input_matrix / col_sums
-    return normalized
+    return np.abs(col_sums)
