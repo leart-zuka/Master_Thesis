83b9d3372b8e1256aeca53413b720425e251f031
Leart Zuka <leart.zuka@web.de>
2025-11-14 15:56:34 +0100

    chore: add note for normalization function

--- code/simulations/phase_gate_simulations/qutip_tries/helpers/generic_computations.py
+++ code/simulations/phase_gate_simulations/qutip_tries/helpers/generic_computations.py
@@ -2,6 +2,11 @@ import numpy as np
 
 
 def normalize_matrix(input_matrix: np.ndarray):
+    """
+    Note: Make sure that all entries are in terms of probabilities and not just raw complex numbers
+          as that can lead to the normalization costant being close to 0, ergo blowing up your term
+          which you're trying to normaliz :>
+    """
     normalized = np.zeros_like(input_matrix)
     normalization_constants = normalizations(input_matrix)
     normalized = input_matrix / normalization_constants
