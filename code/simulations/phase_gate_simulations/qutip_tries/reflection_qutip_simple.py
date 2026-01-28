2cabfe6ff923a0b6b45a8694d6c108f99c0a21c4
Leart Zuka <leart.zuka@web.de>
2026-01-29 00:49:29 +0100

    feat: adjusted plots for simulation results

--- code/simulations/phase_gate_simulations/qutip_tries/reflection_qutip_simple.py
+++ code/simulations/phase_gate_simulations/qutip_tries/reflection_qutip_simple.py
@@ -25,7 +25,7 @@ import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
 tlist = np.linspace(0.0, 8000, 1000)
-args = {"amp": 0.022, "t0": 4000, "tau": 70.0,
+args = {"amp": 0.042, "t0": 4000, "tau": 70.0,
         "tau_start": 91.0, "sigma": 1500.0}
 
 atom = AtomSystem(
@@ -64,88 +64,90 @@ drive = DriveParams(
     args=args,
 )
 
-# out_0_plus, out_0_minus, out_1_plus, out_1_minus, CNOT = (
-#     run_sim_plus_analysis_in_cnot_basis(
-#         tlist=tlist,
-#         cavity=cavity,
-#         atom=atom,
-#         Mu_fc=0.873,
-#         Mu_fr=0.978,
-#         e_obs=e_ops,
-#         c_obs=c_ops,
-#         input_shape=input_shape,
-#         args=args,
-#         system=system,
-#         psi_0=psi_0,
-#         psi_1=psi_1,
-#         post_selection_atom=True,
-#     )
-# )
+out_0_plus, out_0_minus, out_1_plus, out_1_minus, CNOT = (
+    run_sim_plus_analysis_in_cnot_basis(
+        tlist=tlist,
+        cavity=cavity,
+        atom=atom,
+        Mu_fc=0.873,
+        Mu_fr=0.978,
+        e_obs=e_ops,
+        c_obs=c_ops,
+        input_shape=input_shape,
+        args=args,
+        system=system,
+        psi_0=psi_0,
+        psi_1=psi_1,
+        post_selection_atom=True,
+    )
+)
+
+fidelity = compute_signal_fidelity(CNOT)
+print(CNOT)
+print(fidelity)
+exit()
 #
-# fidelity = compute_signal_fidelity(CNOT)
-# print(CNOT)
-# print(fidelity)
-# exit()
-#
-transmissions = np.linspace(0.0, 1.0, 20)
+transmissions = np.linspace(0.0, 1.0, 100)
 fidelities = np.zeros_like(transmissions)
 
-# bw_transmission_fig = plt.figure()
+bw_transmission_fig = plt.figure()
+
+for i, transmission_v in enumerate(transmissions):
+    print(
+        f"Computing Signal Fidelity for transmission of V pol. of: {
+            transmission_v
+        }; Step no. {i}"
+    )
+    cavity = CavitySystem(
+        photon_dim=3,
+        atom_dim=4,
+        Delta_c_pi=2 * np.pi * 0,
+        Delta_c_v=2 * np.pi * 0.5,
+        G0_kc=2 * np.pi * 0.024,
+        Kappa=2 * np.pi * 0.058,
+        v_transmission=transmission_v,
+    )
+    out_0_plus, out_0_minus, out_1_plus, out_1_minus, CNOT = (
+        run_sim_plus_analysis_in_cnot_basis(
+            tlist=tlist,
+            cavity=cavity,
+            atom=atom,
+            Mu_fc=0.873,
+            Mu_fr=0.978,
+            e_obs=e_ops,
+            c_obs=c_ops,
+            input_shape=input_shape,
+            args=args,
+            system=system,
+            psi_0=psi_0,
+            psi_1=psi_1,
+        )
+    )
+    fidelity = compute_signal_fidelity(CNOT)
+    fidelities[i] = fidelity
 #
-# for i, transmission_v in enumerate(transmissions):
-#     print(
-#         f"Computing Signal Fidelity for transmission of V pol. of: {
-#             transmission_v
-#         }; Step no. {i}"
-#     )
-#     cavity = CavitySystem(
-#         photon_dim=3,
-#         atom_dim=4,
-#         Delta_c_pi=2 * np.pi * 0,
-#         Delta_c_v=2 * np.pi * 0.5,
-#         G0_kc=2 * np.pi * 0.024,
-#         Kappa=2 * np.pi * 0.058,
-#         v_transmission=transmission_v,
-#     )
-#     out_0_plus, out_0_minus, out_1_plus, out_1_minus, CNOT = (
-#         run_sim_plus_analysis_in_cnot_basis(
-#             tlist=tlist,
-#             cavity=cavity,
-#             atom=atom,
-#             Mu_fc=0.873,
-#             Mu_fr=0.978,
-#             e_obs=e_ops,
-#             c_obs=c_ops,
-#             input_shape=input_shape,
-#             args=args,
-#             system=system,
-#             psi_0=psi_0,
-#             psi_1=psi_1,
-#         )
-#     )
-#     fidelity = compute_signal_fidelity(CNOT)
-#     fidelities[i] = fidelity
-#
-# plt.plot(transmissions, fidelities, label=r"$F_{signal}$")
-# plt.legend()
-# plt.xlabel("Transmission for V polarization")
-# plt.ylabel("Signal Fidelity")
-# plt.title("Signal Fidelity vs. different transmissions of V polarized light")
-#
-# idx = np.argmax(fidelities)
-# x_max = transmissions[idx]
-# y_max = fidelities[idx]
-# plt.annotate(
-#     f"T = {x_max:.3f}\nF_signal = {y_max:.3f}",
-#     xy=(x_max, y_max),
-#     xytext=(
-#         x_max - 0.3 * (max(fidelities) - min(transmissions)),
-#         y_max - 0.08 * (max(fidelities) - min(fidelities)),
-#     ),
-#     arrowprops=dict(arrowstyle="->", lw=1.5),
-#     fontsize=10,
-#     bbox=dict(boxstyle="round,pad=0.3", fc="white", alpha=0.8),
-# )
+plt.plot(transmissions, fidelities, label=r"$F_{signal}$")
+plt.legend()
+plt.xlabel("Transmission for V polarization")
+plt.ylabel("Signal Fidelity")
+plt.title("Signal Fidelity vs. different transmissions of V polarized light")
+
+idx = np.argmax(fidelities)
+x_max = transmissions[idx]
+y_max = fidelities[idx]
+plt.annotate(
+    f"T = {x_max:.3f}\nF_signal = {y_max:.3f}",
+    xy=(x_max, y_max),
+    xytext=(
+        x_max - 0.3 * (max(fidelities) - min(transmissions)),
+        y_max - 0.08 * (max(fidelities) - min(fidelities)),
+    ),
+    arrowprops=dict(arrowstyle="->", lw=1.5),
+    fontsize=10,
+    bbox=dict(boxstyle="round,pad=0.3", fc="white", alpha=0.8),
+)
+bw_transmission_fig.savefig("Brewster_window_transmission.svg")
+
 # plt.show()
 #
 cavity = CavitySystem(
@@ -159,10 +161,8 @@ cavity = CavitySystem(
 )
 
 
-# args = {"amp": 0.0195, "t0": 4000, "tau": 70.0,
-#         "tau_start": 91.0, "sigma": 1500.0}
-photon_numbers = np.logspace(-4, 1, 20)
-photon_numbers = np.linspace(0.01, 0.05, 5)
+photon_numbers = np.logspace(-4, 1, 100)
+# photon_numbers = np.linspace(0.01, 0.05, 5)
 fidelities = np.zeros_like(photon_numbers)
 
 atomic_state = np.zeros_like(photon_numbers)
@@ -227,6 +227,8 @@ plt.annotate(
     bbox=dict(boxstyle="round,pad=0.3", fc="white", alpha=0.8),
 )
 
+photon_numbers_fig.savefig("photon_numbers.svg")
+
 
 atomic_scattering = plt.figure()
 plt.plot(
@@ -240,13 +242,8 @@ plt.xscale("log")
 plt.ylabel("P(|1>)")
 plt.title("Atomic Scattering vs. Photon Numbers")
 
+atomic_scattering.savefig("atomic_scattering.svg")
 
-# plt.figure()
-# plt.plot(atomic_state, fidelities, "o-")
-# plt.xlabel("Atomic scattering probability")
-# plt.ylabel("Signal fidelity")
-# plt.legend()
-#
 plt.show()
 
 # (
