1161ab52b652a17707fe7f1b9be2a6839aa56a0f
Leart Zuka <leart.zuka@web.de>
2026-02-05 08:33:22 +0100

    chore: remove unneded qt.options

--- code/simulations/phase_gate_simulations/qutip_tries/helpers/compute_simulation.py
+++ code/simulations/phase_gate_simulations/qutip_tries/helpers/compute_simulation.py
@@ -176,9 +176,7 @@ def simulate(
         c_ops,
         e_ops,
         drive.args,
-        options=qt.Options(
-            store_states=True, progress_bar="enhanced", normalize_output=False
-        ),
+        options=qt.Options(store_states=True, progress_bar="enhanced"),
     )
 
     return result
