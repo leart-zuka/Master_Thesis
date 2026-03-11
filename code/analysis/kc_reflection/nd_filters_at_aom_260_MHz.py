71b1063924db7fb13c90c3c123d2725b971cef66
Leart Zuka <leart.zuka@web.de>
2026-03-11 09:53:12 +0100

    feat: nd filters calculation for arb mean photon number (I accidentally set the power to be ideal for a pulse with 0.2 mean photons and didn't wanna change that)

--- code/analysis/kc_reflection/nd_filters_at_aom_260_MHz.py
+++ code/analysis/kc_reflection/nd_filters_at_aom_260_MHz.py
@@ -16,7 +16,9 @@ total_reduction, reduction_1, reduction_2 = calculate_total_reduction(
 
 frequency_2_3 = 384228.115e9  # Rb87 D2 line F=2 <-> F'=3
 frequency_2_1 = 384228.686e9  # Rb87 D2 line F=2 <-> F'1
-mean_photon_number = 0.2
+mean_photon_number = 0.077
+#
+mean_photon_number = 0.4
 # power_reflected_from_cavity = e - 9 * total_reduction
 
 # required_pulse_length = calculate_pulse_length(
@@ -24,5 +26,8 @@ mean_photon_number = 0.2
 # )
 
 required_power = calculate_power(frequency_2_1, 0.2, 1e-6, total_reduction)
-print(f"Required poewr: {Float(required_power * 1 / (0.5532)):.2h}")
-# print(f"Required power: {Float(required_pulse_length):.2h}")
+# print(f"Required poewr: {Float(required_power * 1 / (0.5532)):.2h}")
+required_pulse_length = calculate_pulse_length(
+    frequency_2_1, mean_photon_number, required_power * total_reduction
+)
+print(f"Required power: {Float(required_pulse_length):.2h}")
